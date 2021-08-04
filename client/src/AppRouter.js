be607e9d5b634b4d6d2f6b672426b58f8aac16bf
abran <alexandru.bran@nokia.com>
2021-08-04 10:41:16 +0300

    new build test for error page

--- client/src/AppRouter.js
+++ client/src/AppRouter.js
@@ -11,9 +11,9 @@ export const AppRouter = () => {
   return (
     <HashRouter  >
       <Header basename={config.baseLOCATION} />
-      <Route exact path={config.baseLOCATION + "/error"} component={LoginError} />
-      <Route exact path={config.baseLOCATION + "/"} component={Homepage} />
-    <Route exact path={config.baseLOCATION + "/tac"} component={authGuard(Tac)} />
+        <Route exact path={config.baseLOCATION + "/"} component={Homepage} />
+        <Route exact path={config.baseLOCATION + "/error"} component={LoginError} />
+        <Route exact path={config.baseLOCATION + "/tac"} component={authGuard(Tac)} />
     </HashRouter>
   );
 };
