f44c9b61057f9ec497b7b92b95c0cd09834e5f8e
abran <alexandru.bran@nokia.com>
2021-08-04 09:44:35 +0300

    tac

--- client/src/config.js
+++ client/src/config.js
@@ -13,7 +13,7 @@ const prod = {
 
   config2 = process.env.NODE_ENV === `development` ? dev : prod
   config2.AppName = 'dashboard'
-  config2.appVersion = '1.00'
+  config2.appVersion = '1.01'
 
   export const config = config2
 
