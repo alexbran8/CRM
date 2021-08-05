59a312cfc8f61949c6f91d9dfb462ee0c2ddecf8
abran <alexandru.bran@nokia.com>
2021-08-05 08:56:29 +0300

    redux update

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import "./Header.scss"
-import React, { Component, useState } from "react";
+import React,  {useEffect, useState}  from "react";
+import { useSelector, useDispatch } from "react-redux";
+
 import { config } from "../config"
-import { AUTH_SIGN_UP, AUTH_SIGN_IN, AUTH_SIGN_OUT, AUTH_ERROR } from '../actions/types'
+import { UPDATE_PROFILE, AUTH_SIGN_IN, AUTH_SIGN_OUT, AUTH_ERROR } from '../redux/reducers/types'
 import {
   Collapse,
   Navbar,
@@ -28,90 +30,129 @@ const PopoverContent = () => {
   );
 }
 
-export default class Header extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      firstName: null,
-      lastName: null,
-      loginName: null,
-      isOpen: false
-    }
-    // state = {
-    //   user: {},
-    //   error: null,
-    //   authenticated: false
-    // };
-  }
-  // static propTypes = {
-  //   authenticated: PropTypes.bool.isRequired,
-  //   user: PropTypes.object
-  // };
 
-  componentDidMount() {
-    // Fetch does not send cookies. So you should add credentials: 'include'
-    fetch( config.baseURL + config.baseLOCATION + "/auth/login/success/", {
-      method: "GET",
-      credentials: "include",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Credentials": true
-      }
-    })
-      .then(response => {
-        if (response.status === 200) return response.json();
-        throw new Error("failed to authenticate user");
-      })
-      .then(responseJson => {
-        this.setState({
-          authenticated: true,
-          user: responseJson.user
-        });
+export const Header = () => {
+  const user = useSelector((state) => ({ auth: state.auth }));
+  const [state, setState] = useState([]);
 
-           // save to localstorage and redux
+  const dispatch = useDispatch()
+
+
+
+  useEffect(() => {
+    fetch(config.baseURL + config.baseLOCATION + "/auth/login/success/", {
+         method: "GET",
+         credentials: "include",
+         headers: {
+           Accept: "application/json",
+           "Content-Type": "application/json",
+           "Access-Control-Allow-Credentials": true
+         }
+       })
+         .then(response => {
+           if (response.status === 200) return response.json();
+           throw new Error("failed to authenticate user");
+         })
+         .then(responseJson => {
+           console.log(responseJson)
+           setState({
+             authenticated: true,
+             user: responseJson.user
+           });
            
-           sessionStorage.setItem('userEmail', responseJson.user.email)
-           sessionStorage.setItem('exp', responseJson.user.exp)
-           sessionStorage.setItem('token', responseJson.user.id)
-           sessionStorage.setItem('name', responseJson.user.first_name)
-           sessionStorage.setItem('userEmail', responseJson.user.email)
-           sessionStorage.setItem('roles', responseJson.user.roles)
-
-        console.log(responseJson)
-        return async dispatch => {
+           sessionStorage.setItem('exp',responseJson.user.exp)
+           sessionStorage.setItem('userEmail',responseJson.user.email)
+           sessionStorage.setItem('name',responseJson.user.first_name)
+           sessionStorage.setItem('token',responseJson.user.id)
+           sessionStorage.setItem('roles',responseJson.user.roles)
            dispatch({
-        type: AUTH_SIGN_IN,
-        payload: responseJson.user.id,
-        payload_role: responseJson.user.role,
-        payload_email: responseJson.user.email,
-        payload_name: responseJson.user.first_name,
-        payload_nokiaid: responseJson.user.sub
-      })
-    }
+                     type: UPDATE_PROFILE,
+                     payload: {
+                       role: responseJson.user.roles,
+                       name:responseJson.user.first_name,
+                       email: responseJson.user.email
+                     },
+                 
+                   })
+                  }
+         )
+           .catch(error => {
+                     setState({
+                       authenticated: false,
+                       error: "Failed to authenticate user"
+                     });
+                     console.log(error)
+                   });
+              
+         
+         
+},[])
+//     // Fetch does not send cookies. So you should add credentials: 'include'
+//     fetch( config.baseURL + config.baseLOCATION + "/auth/login/success/", {
+//       method: "GET",
+//       credentials: "include",
+//       headers: {
+//         Accept: "application/json",
+//         "Content-Type": "application/json",
+//         "Access-Control-Allow-Credentials": true
+//       }
+//     })
+//       .then(response => {
+//         if (response.status === 200) return response.json();
+//         throw new Error("failed to authenticate user");
+//       })
+//       .then(responseJson => {
+//         this.setState({
+//           authenticated: true,
+//           user: responseJson.user
+//         });
 
-     
-      })
-      .catch(error => {
-        this.setState({
-          authenticated: false,
-          error: "Failed to authenticate user"
-        });
-        console.log(error)
-      });
-  }
+//            // save to localstorage and redux
+          
+//            sessionStorage.setItem('userEmail', responseJson.user.email)
+//            sessionStorage.setItem('exp', responseJson.user.exp)
+//            sessionStorage.setItem('token', responseJson.user.id)
+//            sessionStorage.setItem('name', responseJson.user.first_name)
+//            sessionStorage.setItem('userEmail', responseJson.user.email)
+//            sessionStorage.setItem('roles', responseJson.user.roles)
 
-  toggle() {
-    this.setState({
-      isOpen: !this.state.isOpen
-    })
-  }
+//         console.log(responseJson)
+//         return async dispatch => {
+//            dispatch({
+//         type: AUTH_SIGN_IN,
+//         payload: responseJson.user.id,
+//         payload_role: responseJson.user.role,
+//         payload_email: responseJson.user.email,
+//         payload_name: responseJson.user.first_name,
+//         payload_nokiaid: responseJson.user.sub
+//       })
+//     }
 
+   // const { authenticated, user } = this.state;
 
+   const _handleSignInClick = () => {
+   // Authenticate using via passport api in the backend
+   // Open Twitter login page
+   // Upon successful login, a cookie session will be stored in the client
+   window.open(config.baseURL + config.baseLOCATION + "/auth/azure", "_self");
+ };
 
-  render() {
-    const { authenticated, user } = this.state;
-    return (
+const  _handleLogoutClick = () => {
+   // Logout using Twitter passport api
+   // Set authenticated state to false in the HomePage
+   window.open(config.baseURL + config.baseLOCATION + "/auth/logout", "_self");
+   sessionStorage.removeItem('exp')
+   sessionStorage.removeItem('userEmail')
+   sessionStorage.removeItem('name')
+   sessionStorage.removeItem('token')
+   sessionStorage.removeItem('roles')
+   handleNotAuthenticated();
+ };
+const  _handleNotAuthenticated = () => {
+   setState({ authenticated: false });
+ };
+   return (
+
       <Navbar className="navbar sticky-nav" expand="sm"  fixed="top">
         <Link className="navbar-brand text-white" id="navbar-brand" to={config.baseLOCATION + "/"}>
           <b>NOKIA</b> {config.AppName} {config.appVersion}
@@ -119,7 +160,10 @@ export default class Header extends Component {
           <PopoverContent  />
       </UncontrolledPopover>
         </Link>
-        <Collapse isOpen={this.state.isOpen} navbar>
+        <Collapse 
+        // isOpen={this.state.isOpen} 
+        navbar>
+          {state.authenticated ?
           <Nav navbar>
             <ul className="navbar-nav text-center">
               <li className="nav-item">
@@ -132,22 +176,23 @@ export default class Header extends Component {
               </li>
             </ul>
           </Nav>
+          : null}
         </Collapse>
         <div className="navbar-text">
           <Nav navbar>
           <li className="nav-item">
-            <Link
+            {/* <Link
               className="nav-link text-white"
               to={config.baseLOCATION + "/signup"}
             >
               Reports
-            </Link>
+            </Link> */}
           </li>
           <ul className="menu">
-              {authenticated ? (
-                <Button color="danger" onClick={this._handleLogoutClick}>Logout {user.email}</Button>
+              {state.authenticated ? (
+                <Button color="danger" onClick={_handleLogoutClick}>Logout {state.user.first_name}</Button>
               ) : (
-                <Button color="primary" onClick={this._handleSignInClick}>Login</Button>
+                <Button color="primary" onClick={_handleSignInClick}>Login</Button>
               )}
             </ul>
           </Nav>
@@ -156,26 +201,5 @@ export default class Header extends Component {
 
     );
   }
-
-  _handleSignInClick = () => {
-    // Authenticate using via passport api in the backend
-    // Open Twitter login page
-    // Upon successful login, a cookie session will be stored in the client
-    window.open(config.baseURL + config.baseLOCATION + "/auth/azure", "_self");
-  };
-
-  _handleLogoutClick = () => {
-    // Logout using Twitter passport api
-    // Set authenticated state to false in the HomePage
-    window.open(config.baseURL + config.baseLOCATION + "/auth/logout", "_self");
-    sessionStorage.removeItem('exp')
-    sessionStorage.removeItem('userEmail')
-    sessionStorage.removeItem('name')
-    sessionStorage.removeItem('token')
-    sessionStorage.removeItem('roles')
-    this.props.handleNotAuthenticated();
-  };
-  _handleNotAuthenticated = () => {
-    this.setState({ authenticated: false });
-  };
-}
+ 
+export default Header
