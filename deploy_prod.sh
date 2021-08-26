faf95a34e68de1c0f16191ebda23f0daa91ee66b
abran <alexandru.bran@nokia.com>
2021-08-26 15:09:44 +0300

    build test

--- deploy_prod.sh
+++ deploy_prod.sh
@@ -15,3 +15,9 @@ docker stop dashboard  && docker rm dashboard
 
 # run new image
 docker run  -d -p 5005:4000  --name dashboard  dashboard 
+
+# remove unused containers
+sudo docker ps -a | grep Exit | cut -d ' ' -f 1 | xargs sudo docker rm
+
+# remove unused images
+docker images -q |xargs docker rmi
