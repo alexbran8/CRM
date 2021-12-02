0515e1ced71bc176fc92bb401c403ebf68da2488
abran <alexandru.bran@nokia.com>
2021-12-02 10:29:20 +0200

    header avatart update

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import "./Header.scss"
-import React,  {useEffect, useState}  from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 
 import { config } from "../config"
@@ -69,7 +69,7 @@ export const Header = () => {
   const [pic, setPic] = useState();
   const dispatch = useDispatch();
 
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const [anchorEl, setAnchorEl] = React.useState < null | HTMLElement > (null);
   const open = Boolean(anchorEl);
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -89,98 +89,57 @@ export const Header = () => {
 
   useEffect(() => {
     fetch(config.baseURL + config.baseLOCATION + "/auth/login/success/", {
-         method: "GET",
-         credentials: "include",
-         headers: {
-           Accept: "application/json",
-           "Content-Type": "application/json",
-           "Access-Control-Allow-Credentials": true
-         }
-       })
-         .then(response => {
-           if (response.status === 200) return response.json();
-           throw new Error("failed to authenticate user");
-         })
-         .then(responseJson => {
-           console.log(responseJson)
-           setState({
-             authenticated: true,
-             user: responseJson.user
-           });
-           sessionStorage.setItem('exp',responseJson.user.exp);
-           sessionStorage.setItem('userEmail',responseJson.user.email);
-           sessionStorage.setItem('userName',responseJson.user.userName);
-           sessionStorage.setItem('name',responseJson.user.first_name);
-           sessionStorage.setItem('token',responseJson.user.token);
-           sessionStorage.setItem('roles',responseJson.user.roles);
-           getIcon(responseJson.user.token);
-           dispatch({
-                     type: UPDATE_PROFILE,
-                     payload: {
-                       role: responseJson.user.roles,
-                       userName:responseJson.user.userName,
-                       name:responseJson.user.first_name,
-                       email: responseJson.user.email
-                     },
-                 
-                   })
-                  }
-         )
-           .catch(error => {
-                     setState({
-                       authenticated: false,
-                       error: "Failed to authenticate user"
-                     });
-                     console.log(error)
-                   });
-              
-         
-         
-},[])
-//     // Fetch does not send cookies. So you should add credentials: 'include'
-//     fetch( config.baseURL + config.baseLOCATION + "/auth/login/success/", {
-//       method: "GET",
-//       credentials: "include",
-//       headers: {
-//         Accept: "application/json",
-//         "Content-Type": "application/json",
-//         "Access-Control-Allow-Credentials": true
-//       }
-//     })
-//       .then(response => {
-//         if (response.status === 200) return response.json();
-//         throw new Error("failed to authenticate user");
-//       })
-//       .then(responseJson => {
-//         this.setState({
-//           authenticated: true,
-//           user: responseJson.user
-//         });
+      method: "GET",
+      credentials: "include",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Credentials": true
+      }
+    })
+      .then(response => {
+        if (response.status === 200) return response.json();
+        throw new Error("failed to authenticate user");
+      })
+      .then(responseJson => {
+        console.log(responseJson)
+        setState({
+          authenticated: true,
+          user: responseJson.user
+        });
+        sessionStorage.setItem('exp', responseJson.user.exp);
+        sessionStorage.setItem('userEmail', responseJson.user.email);
+        sessionStorage.setItem('userName', responseJson.user.userName);
+        sessionStorage.setItem('name', responseJson.user.first_name);
+        sessionStorage.setItem('token', responseJson.user.token);
+        sessionStorage.setItem('roles', responseJson.user.roles);
+        getIcon(responseJson.user.token);
+        dispatch({
+          type: UPDATE_PROFILE,
+          payload: {
+            role: responseJson.user.roles,
+            userName: responseJson.user.userName,
+            name: responseJson.user.first_name,
+            email: responseJson.user.email
+          },
 
-//            // save to localstorage and redux
-          
-//            sessionStorage.setItem('userEmail', responseJson.user.email)
-//            sessionStorage.setItem('exp', responseJson.user.exp)
-//            sessionStorage.setItem('token', responseJson.user.id)
-//            sessionStorage.setItem('name', responseJson.user.first_name)
-//            sessionStorage.setItem('userEmail', responseJson.user.email)
-//            sessionStorage.setItem('roles', responseJson.user.roles)
+        })
+      }
+      )
+      .catch(error => {
+        setState({
+          authenticated: false,
+          error: "Failed to authenticate user"
+        });
+        console.log(error)
+      });
 
-//         console.log(responseJson)
-//         return async dispatch => {
-//            dispatch({
-//         type: AUTH_SIGN_IN,
-//         payload: responseJson.user.id,
-//         payload_role: responseJson.user.role,
-//         payload_email: responseJson.user.email,
-//         payload_name: responseJson.user.first_name,
-//         payload_nokiaid: responseJson.user.sub
-//       })
-//     }
 
-   // const { authenticated, user } = this.state;
 
-   const getIcon = (token) => {
+  }, [])
+
+
+  const getIcon = (token) => {
     console.log(token)
     fetch("https://graph.microsoft.com/v1.0/me/photo/$value", {
       method: "GET",
@@ -206,122 +165,129 @@ export const Header = () => {
   }
 
 
-   const _handleSignInClick = () => {
-   // Authenticate using via passport api in the backend
-   // Open Twitter login page
-   // Upon successful login, a cookie session will be stored in the client
-   window.open(config.baseURL + config.baseLOCATION + "/auth/azure", "_self");
- };
+  const _handleSignInClick = () => {
+    // Authenticate using via passport api in the backend
+    // Open Twitter login page
+    // Upon successful login, a cookie session will be stored in the client
+    window.open(config.baseURL + config.baseLOCATION + "/auth/azure", "_self");
+  };
 
-const  _handleLogoutClick = () => {
-   // Logout using Twitter passport api
-   // Set authenticated state to false in the HomePage
-   window.open(config.baseURL + config.baseLOCATION + "/auth/logout", "_self");
-   sessionStorage.removeItem('exp')
-   sessionStorage.removeItem('userName')
-   sessionStorage.removeItem('userEmail')
-   sessionStorage.removeItem('name')
-   sessionStorage.removeItem('token')
-   sessionStorage.removeItem('roles')
-   handleNotAuthenticated();
- };
-const  _handleNotAuthenticated = () => {
-   setState({ authenticated: false });
- };
-   return (
+  const _handleLogoutClick = () => {
+    // Logout using Twitter passport api
+    // Set authenticated state to false in the HomePage
+    window.open(config.baseURL + config.baseLOCATION + "/auth/logout", "_self");
+    sessionStorage.removeItem('exp')
+    sessionStorage.removeItem('userName')
+    sessionStorage.removeItem('userEmail')
+    sessionStorage.removeItem('name')
+    sessionStorage.removeItem('token')
+    sessionStorage.removeItem('roles')
+    handleNotAuthenticated();
+  };
+  const _handleNotAuthenticated = () => {
+    setState({ authenticated: false });
+  };
+  return (
     <div className={classes.root}>
-    <AppBar position="fixed">
-      <Toolbar>
-        <Typography variant="h6" className={classes.title}>
-        <Link className="navbar-brand text-white" to={"/"}>
-          <b>NOKIA</b> {config.AppName} {user.auth.type === 'student' ? <div className="header-title"> {t("navbar.students")} </div> : null}
-          </Link>
-          <Link className="navbar-brand text-white" to={"/tac"}>TAC</Link>
-        </Typography>
-        {(state && state.authenticated) ? (
-          <div>
-            { pic ?
-            <IconButton
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleMenu}
-              color="inherit"
-            >
-              <Avatar alt="Remy Sharp" src={pic} />
-            </IconButton>
-            : null }
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorEl}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={open}
-              onClose={handleClose}
-            >
-              <MenuItem onClick={_handleLogoutClick}><span title="log out"><ExitToApp /> Log out</span></MenuItem>
-            </Menu>
+      <AppBar position="fixed">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            <Link className="navbar-brand text-white" to={"/"}>
+              <b>NOKIA</b> {config.AppName} {user.auth.type === 'student' ? <div className="header-title"> {t("navbar.students")} </div> : null}
+            </Link>
+            <Link className="navbar-brand text-white" to={"/tac"}>TAC</Link>
+          </Typography>
+          {(state && state.authenticated) ? (
+            <div className="avatar">
+              {pic ?
+                <div className='icon'>
+                  <IconButton
+                    aria-label="account of current user"
+                    aria-controls="menu-appbar"
+                    aria-haspopup="true"
+                    className="icon"
+                    onClick={handleMenu}
+                    color="inherit"
+                  >
+                    <Avatar alt="avatar Sharp"
+                      // className={classes.small}
+                      src={pic} /> <div className='avatar-name'>{state.user.first_name}</div>
+                  </IconButton>
+                </div>
+                : null}
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={open}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={handleClose}>Profile</MenuItem>
+                <MenuItem onClick={_handleLogoutClick}><span title="log out"><ExitToApp /> Log out</span></MenuItem>
+              </Menu>
 
-          </div>
-        ) : (<div><Button variant="contained" color="primary" onClick={_handleSignInClick}><span title="log in">Login</span></Button></div>)}
-      </Toolbar>
-    </AppBar>
-  </div>
+            </div>
 
-      // <Navbar className="navbar sticky-nav" expand="sm"  fixed="top">
-      //   <Link className="navbar-brand text-white" id="navbar-brand" to={config.baseLOCATION + "/"}>
-      //     <b>NOKIA</b> {config.AppName} {config.appVersion}
-      //     <UncontrolledPopover trigger="hover" placement="top" target="navbar-brand">
-      //     <PopoverContent  />
-      // </UncontrolledPopover>
-      //   </Link>
-      //   <Collapse 
-      //   // isOpen={this.state.isOpen} 
-      //   navbar>
-      //     {state.authenticated ?
-      //     <Nav navbar>
-      //       <ul className="navbar-nav text-center">
-      //         <li className="nav-item">
-      //           <Link
-      //             className="nav-link text-white"
-      //             to={"/tac"}
-      //           >
-      //             TAC
-      //                 </Link>
-      //         </li>
-      //       </ul>
-      //     </Nav>
-      //     : null}
-      //   </Collapse>
-      //   <div className="navbar-text">
-      //     <Nav navbar>
-      //     <li className="nav-item">
-      //       {/* <Link
-      //         className="nav-link text-white"
-      //         to={config.baseLOCATION + "/signup"}
-      //       >
-      //         Reports
-      //       </Link> */}
-      //     </li>
-      //     <ul className="menu">
-      //         {state.authenticated ? (
-      //           <Button color="danger" onClick={_handleLogoutClick}>Logout {state.user.first_name}</Button>
-      //         ) : (
-      //           <Button color="primary" onClick={_handleSignInClick}>Login</Button>
-      //         )}
-      //       </ul>
-      //     </Nav>
-      //   </div>
-      // </Navbar>
+          ) : (<div><Button variant="contained" color="primary" onClick={_handleSignInClick}><span title="log in">Login</span></Button></div>)}
+        </Toolbar>
+      </AppBar>
+    </div>
 
-    );
-  }
- 
+    // <Navbar className="navbar sticky-nav" expand="sm"  fixed="top">
+    //   <Link className="navbar-brand text-white" id="navbar-brand" to={config.baseLOCATION + "/"}>
+    //     <b>NOKIA</b> {config.AppName} {config.appVersion}
+    //     <UncontrolledPopover trigger="hover" placement="top" target="navbar-brand">
+    //     <PopoverContent  />
+    // </UncontrolledPopover>
+    //   </Link>
+    //   <Collapse 
+    //   // isOpen={this.state.isOpen} 
+    //   navbar>
+    //     {state.authenticated ?
+    //     <Nav navbar>
+    //       <ul className="navbar-nav text-center">
+    //         <li className="nav-item">
+    //           <Link
+    //             className="nav-link text-white"
+    //             to={"/tac"}
+    //           >
+    //             TAC
+    //                 </Link>
+    //         </li>
+    //       </ul>
+    //     </Nav>
+    //     : null}
+    //   </Collapse>
+    //   <div className="navbar-text">
+    //     <Nav navbar>
+    //     <li className="nav-item">
+    //       {/* <Link
+    //         className="nav-link text-white"
+    //         to={config.baseLOCATION + "/signup"}
+    //       >
+    //         Reports
+    //       </Link> */}
+    //     </li>
+    //     <ul className="menu">
+    //         {state.authenticated ? (
+    //           <Button color="danger" onClick={_handleLogoutClick}>Logout {state.user.first_name}</Button>
+    //         ) : (
+    //           <Button color="primary" onClick={_handleSignInClick}>Login</Button>
+    //         )}
+    //       </ul>
+    //     </Nav>
+    //   </div>
+    // </Navbar>
+
+  );
+}
+
 export default Header
