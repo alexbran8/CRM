6afafbd110ff776c4737d0090364b7ecacdb42cc
abran <alexandru.bran@nokia.com>
2022-01-07 15:13:35 +0200

    draft TT no.

--- client/src/components/ReportsModal/ReportsModal.tsx
+++ client/src/components/ReportsModal/ReportsModal.tsx
@@ -4,15 +4,19 @@ export const ReportsModalBody = (props) => {
 const [data, setData] = useState();
 
   function groupAndSum(arr, groupKeys, sumKeys){
-    return Object.values(
+    return Object.values( 
       arr.reduce((acc,curr)=>{
         const group = groupKeys.map(k => curr[k]).join('-');
         acc[group] = acc[group] || Object.fromEntries(
            groupKeys.map(k => [k, curr[k]]).concat(sumKeys.map(k => [k, 0])));
         sumKeys.forEach(k => acc[group][k] += curr[k]);
+        acc[group]['no_incident'] += curr['no_incident'] + ', '       
+        // acc[group]['comment'] = arr.filter(item=> item.responsible_entity = curr.responsible_entity && item.date == curr.date).reduce((prev, current)=>{ prev + '-' + current})
+        // console.log(arr.filter(item=> item.responsible_entity = curr.responsible_entity && item.date == curr.date).reduce((prev, current)=>{ return current.no_incident}))
+        // console.log(acc[group]['comment'])
         return acc;
       }, {})
-    );
+    )
   }
 
   function groupCalculateHours (data) {
@@ -26,7 +30,6 @@ const [data, setData] = useState();
   // get props data and group by  user and date
   useEffect(()=> {
     groupCalculateHours(props.data)
-    console.log(props.data)
     let grouppedData = groupAndSum(props.data, ['responsible_entity', 'NORM','date'], ['duration'])
     setData(grouppedData)
 
@@ -34,7 +37,7 @@ const [data, setData] = useState();
   return (
     <>
       <div>
-        {data && data.map(item => { return <div>{item.responsible_entity},{item.date}, {item.NORM}, {item.duration} </div> })}
+        {data && data.map(item => { return <div>{item.responsible_entity},{item.date}, {item.NORM}, {item.no_incident}, {item.duration} </div> })}
       </div>
     </>
   )
