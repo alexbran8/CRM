03ceeb0bf78db3fa06e96f5b312f0ee68feabb1e
abran <alexandru.bran@nokia.com>
2022-01-08 09:40:55 +0200

    draft copy to clipboard

--- client/src/components/ReportsModal/ReportsModal.tsx
+++ client/src/components/ReportsModal/ReportsModal.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 
 export const ReportsModalBody = (props) => {
   const [data, setData] = useState();
-  const [total, setTotal] = useState<Float>(0)
+  const [total, setTotal] = useState < Float > (0)
 
   function groupAndSum(arr, groupKeys, sumKeys) {
     return Object.values(
@@ -10,14 +10,14 @@ export const ReportsModalBody = (props) => {
         const group = groupKeys.map(k => curr[k]).join('-');
         acc[group] = acc[group] || Object.fromEntries(
           groupKeys.map(k => [k, curr[k]]).concat(sumKeys.map(k => [k, 0])));
-          console.log('test1',acc[group]['comment2'])
-          // acc[group]['comment2'] = null
+        console.log('test1', acc[group]['comment2'])
+        // acc[group]['comment2'] = null
         sumKeys.forEach(k => acc[group][k] += curr[k]);
         // acc[group]['comment'] += '#T2 ' + curr['duration'] + ': ' + curr['no_incident'] + ', '
-        acc[group]['comment1'] =  '#' + curr['NORM'] +'(' + curr['duration'] + '): ';
-        console.log('test',acc[group]['comment2'])
+        acc[group]['comment1'] = '#' + curr['NORM'] + '(' + curr['duration'] + '): ';
+        console.log('test', acc[group]['comment2'])
         // acc[group]['comment2'] = 
-        curr['no_incident'] !== undefined ? acc[group]['comment2'] +=  curr['no_incident'] + ', ' : null
+        curr['no_incident'] !== undefined ? acc[group]['comment2'] += curr['no_incident'] + ', ' : null
 
         return acc;
       }, {})
@@ -43,13 +43,28 @@ export const ReportsModalBody = (props) => {
     <>
       total hours: {total}
       <table>
-        {data && data.map((item, index) => { return <tr key={index}>
-          <td>{item.responsible_entity}</td>
-          <td>{item.date}</td> 
-          <td>{item.task}</td> 
-          <td>{item.NORM}</td> 
-          <td>{item.comment1 + '-' + item.comment2.replace("undefined","")}</td> 
-        <td>{item.duration}</td> </tr> })}
+        {data && data.map((item, index) => {
+          return <tr key={index}>
+            <td>{item.responsible_entity}</td>
+            <td>{item.date}</td>
+            <td>{item.task}</td>
+            {/* <td>{item.NORM}</td>  */}
+            <textarea
+            cols="100"
+            // disabled={true}
+              onClick={() => navigator.clipboard.writeText(item.comment1 + '-' + item.comment2.replace("undefined", ""))}
+            >
+              {item.comment1 + '-' + item.comment2.replace("undefined", "")}
+            </textarea>
+            <textarea
+            cols="5"
+            // disabled={true}
+              onClick={() => navigator.clipboard.writeText(item.duration.toFixed(2))}
+            >
+              {item.duration.toFixed(2)}
+            </textarea>
+            </tr>
+        })}
       </table>
     </>
   )
