2d7f4f4726404e9164d8ea9ebf7de76f7ab99907
abran <alexandru.bran@nokia.com>
2022-01-26 19:17:57 +0200

    new test

--- client/src/AppRouter.js
+++ client/src/AppRouter.js
@@ -2,11 +2,11 @@ import React from "react";
 import Homepage from "./components/Homepage.jsx";
 import Header from "./components/Header";
 import LoginError from "./components/LoginError";
-// import Tac from "./components/Tacdb";
+import TAC from "./components/TAC/TAC";
 // import {PIP} from "./components/PIP/PIP";
 import ErrorBoundary from "./HOCs/ErrorBoundary.tsx";
-const TAC = process.env.NODE_ENV == "development" ? React.lazy(() => import(/* webpackChunkName: "tacdbchunk" */ './components/TAC/TAC')) : React.lazy(() => import(/* webpackChunkName: "/tacdbchunk" */ './components/TAC/TAC'));
-const PIP = process.env.NODE_ENV == "development" ? React.lazy(() => import(/* webpackChunkName: "pip" */ './components/PIP/PIP')) : React.lazy(() => import(/* webpackChunkName: "/APage" */ './components/PIP/PIP'));
+// const TAC = process.env.NODE_ENV == "development" ? React.lazy(() => import(/* webpackChunkName: "tacdbchunk" */ './components/TAC/TAC')) : React.lazy(() => import(/* webpackChunkName: "/tacdbchunk" */ './components/TAC/TAC'));
+const PIP = process.env.NODE_ENV == "development" ? React.lazy(() => import(/* webpackChunkName: "pip" */ './components/PIP/PIP')) : React.lazy(() => import(/* webpackChunkName: "/pip" */ './components/PIP/PIP'));
 // const PIP = React.lazy(() => import(/* webpackChunkName: "APage" */ './components/PIP/PIP'));
 import { HashRouter, Route, Switch } from "react-router-dom";
 import { config } from "./config"
@@ -24,7 +24,8 @@ export const AppRouter = () => {
           <Header basename={config.baseLOCATION} />
           <Route exact path={"/"} component={Homepage} />
           <Route exact path={"/error"} component={LoginError} />
-          {user.auth.role === 'L3' || user.auth.role === 'login' ? <Route exact path={"/tac"} component={authGuard(TAC)} /> : null}
+          <Route exact path={"/tac"} component={authGuard(TAC)} />
+          {/* {user.auth.role === 'L3' || user.auth.role === 'login' ? <Route exact path={"/tac"} component={authGuard(TAC)} /> : null} */}
           {user.auth.role === 'L3' || user.auth.role === 'PIP' ? <Route exact path={"/prod-indus-planning"} component={authGuard(PIP)} /> : null}
         </HashRouter>
         </ErrorBoundary>
