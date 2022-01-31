91703c9c304aa9827550c3b0a5666246a8fd0a81
abran <alexandru.bran@nokia.com>
2022-01-31 09:42:47 +0200

    task type update

--- client/src/components/TAC/types.tsx
+++ client/src/components/TAC/types.tsx
@@ -1,1 +1,5 @@
-export const appelList = ["SSNE", "Appel non destiné  TAC", "CLA", "Assigné", "Appel", "LdR", "Pre-check", "Suivi",  "Taskforce", "Taskforce RdP","VIGIE NOK"]
+export const appelList = ["SSNE", "Appel non destiné  TAC", "CLA", "Assigné", "Appel", "LdR",
+//  "Pre-check",
+  "Suivi",  
+// "Taskforce", "Taskforce RdP",
+"VIGIE NOK"]
