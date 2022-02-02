2c08b3ab6f454506d304e7aabebf07dd3818d57c
abran <alexandru.bran@nokia.com>
2022-02-02 12:07:31 +0200

    pip.js

--- server/graphql/schemas/index.js
+++ server/graphql/schemas/index.js
@@ -2,7 +2,7 @@ const { gql } = require("apollo-server");
 
 const tacdb = require("./tacdb");
 // second app => see how to make this dynamic
-const pip = require("./pip")
+const pip = require("./pip.js")
 
 const rootType = gql`
   type Query {
